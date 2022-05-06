373293b9ef3a44faff44f7c39a45baa2fd415fcd
Prathamesh Punde⚡ <54547411+prathamesh-2002@users.noreply.github.com>
2022-05-06 14:27:49 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (37.01%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int ctr = 0;
+        while(num != 0) {
+            num = (num%2 == 0) ? num/2 : num - 1;
+            ctr++;
+        }
+        return ctr;
+    }
+}
